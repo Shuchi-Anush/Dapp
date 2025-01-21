d8643c72af63b3e9796c5749bd1d1d41f7977b22
dinesh <dinesh.official1305@gmail.com>
2025-01-22 00:50:26 +0530

    Basis template for connecting the front with smart contract

--- user/src/App.jsx
+++ user/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState,useEffect } from 'react'
-
+import abi from "./Json/Twitter.json"; 
 import './App.css'
-
+import {ethers} from "ethers"
 
 
 function App() {
@@ -14,20 +14,34 @@ function App() {
   useEffect(()=>{
     const template=async()=>{
    
-      const contractAddres="";
-      const contractABI="";
+      const contractAddress="0x8bFD84Db85392f7E509B343C0FC8A9a211892451";
+      const contractABI=abi.abi;
       //Metamask part
       //1. In order do transactions on goerli testnet
       //2. Metmask consists of infura api which actually help in connectig to the blockhain
     
-
+   try{
         const {ethereum}=window;
         const account = await ethereum.request({
           method:"eth_requestAccounts"
         })
  
-      
+      const provider=new ethers.providers.Web3Provider(ethereum);//read the bock
+      const signer = provider.getSigner();
+      const contract =new ethers.Contract(
+        contractAddress,
+        contractABI,
+        signer
+      )
+      console.log(contract)
+      setState({provider,signer,contract});
     }
+    catch(error){
+alert(error);
+    }
+  }
+    
+
     template();
   },[])
   return (
